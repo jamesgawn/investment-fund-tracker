86744789826b8ea7c7965f5baddbc15395688bd0
jamesgawn <james@gawn.uk>
2020-10-22 18:18:56 +0100

    Added stubs and expect for holding valuation lambda

--- src/lambda.spec.ts
+++ src/lambda.spec.ts
@@ -4,19 +4,21 @@ import {Fund} from "./domain/Fund";
 
 const mockGetRecords = jest.fn();
 const mockPutRecord = jest.fn();
-// jest.mock("./lib/DynamoDBHelper", () => ({
-//   DynamoDBHelper: jest.fn().mockImplementation(() => ({
-//     getRecords: mockGetRecords,
-//     putRecord: mockPutRecord
-//   }))
-// }));
+const mockQueryRecordsByKey = jest.fn();
+jest.mock("./lib/DynamoDBHelper", () => ({
+  DynamoDBHelper: jest.fn().mockImplementation(() => ({
+    getRecords: mockGetRecords,
+    putRecord: mockPutRecord,
+    queryRecordByKey: mockQueryRecordsByKey
+  }))
+}));
 
 const mockGetFund = jest.fn();
-// jest.mock("./lib/SecurityRetriever", () => ({
-//   SecurityRetriever: jest.fn().mockImplementationOnce(() => ({
-//     getFundPrice: mockGetFund
-//   }))
-// }));
+jest.mock("./lib/SecurityRetriever", () => ({
+  SecurityRetriever: jest.fn().mockImplementationOnce(() => ({
+    getFundPrice: mockGetFund
+  }))
+}));
 
 describe("lambda", () => {
   const testFund = new Fund("GB0001", "Bingo Bob Fund");
@@ -45,11 +47,29 @@ describe("lambda", () => {
       amount: 100,
       price: 50
     };
+    const price = {
+      isin: "GB001",
+      name: "blah",
+      price: 55,
+      date: ""
+    };
     beforeEach(() => {
       mockGetRecords.mockResolvedValue([testFundHolding]);
+      mockQueryRecordsByKey.mockResolvedValue([price]);
     });
     test("should run successfully", async () => {
-      expect(holdingValuationHandler(event as any, context as any, {} as any)).resolves.not.toThrow();
+      expect(holdingValuationHandler(event as any, context as any, {} as any)).resolves.toEqual([{
+        date: "2020-01-01",
+        isin: "GB001",
+        name: "blah",
+        amount: 100,
+        originalPrice: 50,
+        originalValue: 50,
+        currentPrice: 55,
+        currentValue: 55,
+        profitValue: 5,
+        profitPercentage: 0.09
+      }]);
     });
   });
 });
